25b462dc1f79d83ef055c0854a2525376ef0d10f
nnewsom <nnewsom@sunera.com>
2013-07-19 13:23:03 +0000

    added header

--- classify_webbies/classify.webbies.py
+++ classify_webbies/classify.webbies.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+#    Copyright (C) 2013 Sunera, LLC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    #####################################################################
+#    Author:            Nick Newsom   
+#    Blog:              http://security.sunera.com/
+#
+#    Program Name:      classify.webbies.py
+#    Purpose:           Enumerate and screenshot web services
+#    Version:           1.0
+#    Code Repo:         http://code.google.com/p/sunera-ap-team/
 
 import sys,re,httplib,socket,signal,argparse,time,os
 from netaddr import IPNetwork,IPAddress
