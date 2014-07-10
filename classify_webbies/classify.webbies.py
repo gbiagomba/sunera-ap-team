bda7e4030d52289e84eff6d56913511ebfb6a81e
newt <nnewsom@sunera.com>
2014-07-10 14:34:04 -0400

    added legal stuffs

--- classify_webbies/classify.webbies.py
+++ classify_webbies/classify.webbies.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python2.7
+
+#    Copyright (C) 2014 Sunera, LLC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    #####################################################################
+#    Author:            Nick Newsom
+#    Blog:              http://security.sunera.com/
+#
+#    Program Name:      classify.webbies.py
+#    Purpose:           Enumerate and screenshot web services
+#    Version:           4.1
+#    Code Repo:         http://code.google.com/p/sunera-ap-team/
+
 import sys,argparse,os
 import cPickle as pickle
 from random import choice
