db0a3c9a2f7ccc5505b5a49e83a609d95743e7b1
nnewsom <nnewsom@sunera.com>
2015-02-03 22:57:57 +0000

    license stuff

--- classify_webbies/fdb.py
+++ classify_webbies/fdb.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python2.7
+#    Copyright (C) 2015 Sunera, LLC
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    #####################################################################
+#    Author:            Nick Newsom
+#    Blog:              http://security.sunera.com/
+#
+#    Program Name:      fdb.py
+#    Purpose:           dirbust mass amounts of web services
+#    Version:           1.0
+#    Code Repo:         http://code.google.com/p/sunera-ap-team/
+
 from gevent import monkey
 monkey.patch_all()
 from gevent.pool import Pool
